5255387e6f0a8a12d526ee92b0b75489edd75b69
Joshua-Gabriel Camacho <shua.camacho@gmail.com>
2022-12-13 19:48:27 -0500

    Add event listener

--- logic.js
+++ logic.js
@@ -1,11 +1,16 @@
 //global variables
 
-
 //builds a grid
 
 for (let i = 0; i < 256; i++) {
-    const grid = document.createElement('div');
-    const canvas = document.querySelector('#canvas');
-    grid.classList.add('grid')
-    canvas.appendChild(grid)
+  const grid = document.createElement("div");
+  const canvas = document.querySelector("#canvas");
+  grid.classList.add("grid");
+  canvas.appendChild(grid);
 }
+
+//event listener to wait for hover
+const grid = document.getElementsByClassName('grid')
+grid.addEventListener('mouseover', addColour(colour))
+
+//function that adds and selects colour
